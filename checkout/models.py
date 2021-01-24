a78791f99c8a5679378882e6a3f0aba8743f50a0
Evert <evert.rot@gmx.com>
2021-01-24 20:34:27 +0100

    updated orders object

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,7 @@
+import uuid
+
 from django.db import models
+from django.db.models import Sum
 from django.contrib.auth.models import User
 from works.models import work_items
 
@@ -6,12 +9,17 @@ from works.models import work_items
 
 
 class orders(models.Model):
+    order_number = models.CharField(max_length=32, null=False) 
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
     user = models.ForeignKey(
         User, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateTimeField(blank=True, null=True)    
     paid = models.BooleanField(default=False)
+    pay_method = models.CharField(blank=True, max_length=10)    
     sent = models.BooleanField(default=False)
-    sent_date = models.DateField(blank=True, null=True)
+    sent_date = models.DateTimeField(blank=True, null=True)
+    sent_method = models.CharField(blank=True, max_length=10)
+    sent_tracking = models.CharField(blank=True, max_length=254)
     total = models.DecimalField(
         max_digits=5, decimal_places=2, default=0)
     address1 = models.CharField(blank=True, max_length=50)
@@ -24,16 +32,17 @@ class orders(models.Model):
         max_digits=5, decimal_places=2, default=0)
 
     def __str__(self):
-        return "{0}-{1}-{2}".format(self.id, self.date, self.user.last_name)
+        return self.order_number       
 
 
 class order_items(models.Model):
-    order = models.ForeignKey(orders, null=False, on_delete=models.CASCADE)
+    order = models.ForeignKey(orders, null=False, on_delete=models.CASCADE, related_name='orderitems')
     work_item = models.ForeignKey(
         work_items, null=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(blank=False)
     price = models.DecimalField(
         max_digits=5, decimal_places=2, default=0)
+
 
     def __str__(self):
         return "{0} {1} @ {2}".format(
